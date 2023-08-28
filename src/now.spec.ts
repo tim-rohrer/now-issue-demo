5a5ff652f233848e370c96ecf889ebc8f8f7430d
tim.rohrer <tim@tgr66.dev>
2023-08-27 17:47:08 -0800

    fix: Remove types from let statements

--- src/now.spec.ts
+++ src/now.spec.ts
@@ -1,15 +1,15 @@
-import { Db, MongoClient } from "mongodb"
+import { MongoClient } from "mongodb"
 
 import { jest } from "@jest/globals"
 
-let connection: MongoClient
-let db: Db
+let connection
+let db
 
 jest.useFakeTimers({ doNotFake: ["nextTick", "setImmediate"] })
 
 beforeAll(async () => {
   connection = await MongoClient.connect(globalThis.__MONGO_URI__)
-  db = await connection.db(globalThis.__MONGO_DB_NAME__)
+  db = connection.db(globalThis.__MONGO_DB_NAME__)
 })
 
 afterAll(async () => {
@@ -18,14 +18,11 @@ afterAll(async () => {
 
 test("Demo $$NOW doesn't use mocked system time", async () => {
   const db = connection.db("test1")
-
   const mockedTime = new Date("2023-08-01T12:00:00.000Z")
   jest.setSystemTime(mockedTime)
-
   await db.collection("events").insertOne({
     now1: new Date(),
   })
-
   const result = (
     await db
       .collection("events")
@@ -43,6 +40,4 @@ test("Demo $$NOW doesn't use mocked system time", async () => {
 
   expect(result.now1).toEqual(mockedTime) // Passes
   expect(result.now2).toEqual(mockedTime) // Fails
-
-  connection.close()
 })
